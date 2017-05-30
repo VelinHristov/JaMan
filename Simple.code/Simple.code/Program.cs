3b464d1bd09652ceff7f10e110b3512a0fa3276c
VelinHristov <resingdora@abv.bg>
2017-05-30 16:34:30 +0300

    firstcommit

---
+++ Simple.code/Simple.code/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Simple.code
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+			Console.WriteLine ("Simple code");
+
+
+		}
+	}
+}
